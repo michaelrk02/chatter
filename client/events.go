59cd5c228c92ca8011a66c0775da52b5e9248034
Michael Krisnadhi <michaelkrisnadhi@gmail.com>
2020-05-22 12:02:02 +0700

    restructure event handling

--- client/events.go
+++ client/events.go
@@ -2,8 +2,11 @@ package main
 
 import (
     "context"
+    "errors"
     "fmt"
     "github.com/golang/protobuf/ptypes/wrappers"
+    "github.com/michaelrk02/chatter"
+    "google.golang.org/grpc"
     "google.golang.org/grpc/codes"
     "google.golang.org/grpc/status"
     "io"
@@ -11,87 +14,81 @@ import (
     "time"
 )
 
-func pollClientConnectEvents() {
+type eventCallback func(evt interface{})
+
+func pollEvents(event string, callback eventCallback) {
     var err error
-    stream, err := chatterEventsCln.ListenClientConnect(context.Background(), &wrappers.UInt32Value{Value: clientId})
+    var stream interface{}
+    var data interface{}
+    arg := &wrappers.UInt32Value{Value: clientId}
+    switch event {
+    case "ClientConnect":
+        stream, err = chatterEventsCln.ListenClientConnect(context.Background(), arg)
+        data = new(chatter.ClientConnectionEvent)
+    case "ClientDisconnect":
+        stream, err = chatterEventsCln.ListenClientDisconnect(context.Background(), arg)
+        data = new(chatter.ClientConnectionEvent)
+    case "ClientMessage":
+        stream, err = chatterEventsCln.ListenClientMessage(context.Background(), arg)
+        data = new(chatter.ClientMessageEvent)
+    case "ServerMessage":
+        stream, err = chatterEventsCln.ListenServerMessage(context.Background(), arg)
+        data = new(chatter.ServerMessageEvent)
+    }
     if err != nil {
         panic(err)
     }
     for atomic.LoadInt32(&running) == 1 {
-        evt, err := stream.Recv()
+        err = stream.(grpc.ClientStream).RecvMsg(data)
         if err != nil {
             if err == io.EOF || status.Code(err) == codes.NotFound {
+                break
+            } else if status.Code(err) == codes.Unavailable {
+                if atomic.LoadInt32(&running) == 1 {
+                    panic(errors.New("server shutdown"))
+                }
                 break
             } else {
                 panic(err)
             }
         }
+        callback(data)
+    }
+}
+
+func pollClientConnectEvents() {
+    pollEvents("ClientConnect", func(data interface{}) {
+        evt := data.(*chatter.ClientConnectionEvent)
         outMu.Lock()
         fmt.Printf("[%s] %s has entered the chat\n", time.Unix(evt.Timestamp, 0).Local().Format(time.Kitchen), evt.Nickname)
         outMu.Unlock()
-    }
+    })
 }
 
 func pollClientDisconnectEvents() {
-    var err error
-    stream, err := chatterEventsCln.ListenClientDisconnect(context.Background(), &wrappers.UInt32Value{Value: clientId})
-    if err != nil {
-        panic(err)
-    }
-    for atomic.LoadInt32(&running) == 1 {
-        evt, err := stream.Recv()
-        if err != nil {
-            if err == io.EOF || status.Code(err) == codes.NotFound {
-                break
-            } else {
-                panic(err)
-            }
-        }
+    pollEvents("ClientDisconnect", func(data interface{}) {
+        evt := data.(*chatter.ClientConnectionEvent)
         outMu.Lock()
         fmt.Printf("[%s] %s has left the chat\n", time.Unix(evt.Timestamp, 0).Local().Format(time.Kitchen), evt.Nickname)
         outMu.Unlock()
-    }
+    })
 }
 
 func pollClientMessageEvents() {
-    var err error
-    stream, err := chatterEventsCln.ListenClientMessage(context.Background(), &wrappers.UInt32Value{Value: clientId})
-    if err != nil {
-        panic(err)
-    }
-    for atomic.LoadInt32(&running) == 1 {
-        evt, err := stream.Recv()
-        if err != nil {
-            if err == io.EOF || status.Code(err) == codes.NotFound {
-                break
-            } else {
-                panic(err)
-            }
-        }
+    pollEvents("ClientMessage", func(data interface{}) {
+        evt := data.(*chatter.ClientMessageEvent)
         outMu.Lock()
         fmt.Printf("[%s] %s: %s\n", time.Unix(evt.Timestamp, 0).Local().Format(time.Kitchen), evt.Nickname, evt.Contents)
         outMu.Unlock()
-    }
+    })
 }
 
 func pollServerMessageEvents() {
-    var err error
-    stream, err := chatterEventsCln.ListenServerMessage(context.Background(), &wrappers.UInt32Value{Value: clientId})
-    if err != nil {
-        panic(err)
-    }
-    for atomic.LoadInt32(&running) == 1 {
-        evt, err := stream.Recv()
-        if err != nil {
-            if err == io.EOF || status.Code(err) == codes.NotFound {
-                break
-            } else {
-                panic(err)
-            }
-        }
+    pollEvents("ServerMessage", func(data interface{}) {
+        evt := data.(*chatter.ServerMessageEvent)
         outMu.Lock()
         fmt.Printf("[%s] SERVER: %s\n", time.Unix(evt.Timestamp, 0).Local().Format(time.Kitchen), evt.Contents)
         outMu.Unlock()
-    }
+    })
 }
 
